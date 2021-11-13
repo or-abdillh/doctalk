fa413787dd9d47387818b0b6e324d9fa77f6dcd8
or-abdillh <hans.abdillh05@gmail.com>
2021-11-13 23:16:23 +0800

    Create Appointment.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
 
   <Header></Header>
+  <Appointment></Appointment>
   
 </template>
 
 <script setup>
 
   import Header from './components/Header.vue'
+  import Appointment from './components/Appointment.vue'
 
 </script>
---
+++ src/components/Appointment.vue
@@ -0,0 +1,42 @@
+<template>
+   <section class="px-5 mt-5">
+      <div class="flex justify-between items-center">
+         <span class="text-blue-500 text-sm font-medium">
+            My Appointment
+         </span>
+         <span class="text-gray-400 text-xs font-medium">
+            Today
+         </span>
+      </div>
+      
+      <div class="mt-3 bg-primary p-3 rounded-lg">
+         <div class="flex justify-between">
+            <div class="flex gap-2">
+               <img src="/profile.jpg" class="rounded-lg" width="60" />
+               <div class="text-gray-100">
+                  <strong class="text-xs">Dr. Amanda Kirana</strong>
+                  <p class="text-xxs">Psychologist</p>
+               </div>
+            </div>
+            <div>
+               <span class="bg-green-400 px-2 py-1 rounded-lg grid place-items-center">
+                  <i class="text-gray-50 text-sm fas fa-phone-alt"></i>
+               </span>
+            </div>
+         </div>
+         <div class="bg-blue-300 mt-3 flex justify-between items-center text-gray-300 rounded-lg bg-opacity-50 py-1 px-3">
+            <span class="text-xxs gap-2">
+               <i class="text-xxs fa fa-clock"></i>
+               <small>
+                  12:00 PM
+               </small>
+            </span>
+            <span class="text-xxs">
+               <i class="text-xxs mr-1 fas fa-calendar-alt"></i>
+               <small>Friday, 29 September 2021</small>
+            </span>
+         </div>
+      </div>
+      
+   </section>
+</template>
