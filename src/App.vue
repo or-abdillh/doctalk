ee75acc91ae1c5bb6ceccf36d3d1840a8031fa45
or-abdillh <hans.abdillh05@gmail.com>
2021-11-14 10:54:56 +0800

    Create Navbar component and update Nearby component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <Header></Header>
   <Appointment></Appointment>
   <Nearby></Nearby>
+  <Navbar></Navbar>
   
 </template>
 
@@ -11,5 +12,6 @@
   import Header from './components/Header.vue'
   import Appointment from './components/Appointment.vue'
   import Nearby from './components/Nearby.vue'
+  import Navbar from './components/Navbar.vue'
 
 </script>
